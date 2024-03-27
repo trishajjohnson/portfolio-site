dc6c601c134ed35d81627936976ea949411f15dd
Trish <trishajjohnson@gmail.com>
2024-03-26 21:45:25 -0700

    update url for direct download

--- components/ResumeBanner/ResumeButton/index.js
+++ components/ResumeBanner/ResumeButton/index.js
@@ -6,7 +6,7 @@ export default function ResumeButton() {
 
     function handleDownload() {
         const aTag = document.createElement("a");
-        aTag.href = "https://www.dropbox.com/scl/fi/bwfeubvl73i75kgaawzn7/Trisha-Johnson_Resume.pdf?rlkey=qx0xpc6x6x4g9vg7k4n55i2zt&dl=0";
+        aTag.href = "https://www.dropbox.com/scl/fi/bwfeubvl73i75kgaawzn7/Trisha-Johnson_Resume.pdf?rlkey=qx0xpc6x6x4g9vg7k4n55i2zt&dl=1";
         aTag.setAttribute("download", "Trisha Johnson_Resume.pdf");
         document.body.appendChild(aTag);
         aTag.click();
